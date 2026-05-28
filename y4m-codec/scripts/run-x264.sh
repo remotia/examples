cfb6a32f62708bf71aedc28f896b24dfad556d95
aegroto <aegroto@protonmail.com>
2026-05-28 16:15:21 +0200

    chore: simpler script

---
+++ y4m-codec/scripts/run-x264.sh
@@ -0,0 +1,14 @@
+Y4M_INPUT="$1"
+OUTPUT_DIR="${2:-sample-runs}"
+
+mkdir -p "$OUTPUT_DIR"
+basename=$(basename "$Y4M_INPUT" .y4m)
+H264_OUTPUT="${OUTPUT_DIR}/${basename}_x264.h264"
+DECODED_DIR="${OUTPUT_DIR}/${basename}_x264_decoded"
+
+cargo run --release --bin y4m-encoder -- \
+    -i "$Y4M_INPUT" -o "$H264_OUTPUT" \
+    --codec libx264 --option "crf=23" --option "preset=medium" --option "tune=film"
+
+cargo run --release --bin y4m-decoder -- \
+    -i "$H264_OUTPUT" -o "$DECODED_DIR" -W 1920 -H 1080 --codec h264
